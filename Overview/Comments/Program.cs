afa7f4a75247b598406729ed1ffdac8e59cd458d
Shubham Shubham <shubham4315@gmail.com>
2023-02-21 15:04:07 +0100

    Generate xml documentation from documentation

--- Overview/Comments/Program.cs
+++ Overview/Comments/Program.cs
@@ -6,6 +6,13 @@ namespace Comments
     {
         /// XML Comments are used to help provide documentation
         /// They start with triple-slashes and have a special syntax
+        /// <summary>
+        /// This is the main simple application
+        /// </summary>
+        /// <param name="args"> array os string argumnts </param>
+        /// <returns>
+        /// Nothing
+        /// </returns> 
         static void Main(string[] args)
         {
 
