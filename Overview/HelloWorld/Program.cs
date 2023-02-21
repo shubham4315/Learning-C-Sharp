a973353c37a2d0807a89711e524f36a4b0368828
Shubham Shubham <shubham4315@gmail.com>
2023-02-21 14:16:46 +0100

    We can do the same thing with this code without the boilerplate

--- Overview/HelloWorld/Program.cs
+++ Overview/HelloWorld/Program.cs
@@ -1,18 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System;
-
-namespace MyApp // Note: actual namespace depends on the project name.
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("What is your Name");
-            string str = Console.ReadLine();
-            Console.WriteLine("Hello " + str);
-        }
-    }
-}
-
+Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your Name");
+string str = Console.ReadLine();
+Console.WriteLine("Hello " + str);
