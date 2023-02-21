072aeb816df349e3e2e64b527664bc1cc348314c
Shubham Shubham <shubham4315@gmail.com>
2023-02-21 16:29:56 +0100

    Number guess game

--- Strings/ProgChallengeStart/Program.cs
+++ Strings/ProgChallengeStart/Program.cs
@@ -14,6 +14,33 @@ namespace ProgChallengeStart
             Console.WriteLine("I'm thinking of a number between 0 and 20.");
             Console.WriteLine("Enter your guess, or -1 to give up.");
 
+            int guessCount = 0;
+
+            int guess = -1;
+            bool succeeded = false;
+            
+            succeeded = Int32.TryParse(Console.ReadLine(), out guess);
+            do{
+                if(succeeded){
+                    if (guess > theNumber)
+                    {
+                        Console.WriteLine("Nope, Lower than that");
+                    }
+                    else if (guess < theNumber)
+                    {
+                        Console.WriteLine("Nope, Higher than that");
+                    }
+                }
+                else{
+                    Console.WriteLine("Opps, try a number");
+                }
+                guessCount++;
+                succeeded = Int32.TryParse(Console.ReadLine(), out guess);
+
+            }while(guess != theNumber);
+
+            Console.WriteLine($"You took {guessCount} tries");
+
             // Keep track of the number of guesses and the current user guess
 
             // Start the game and run until user quits or guesses correctly
