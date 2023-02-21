e165921a43b45401950528f4f7efbfbc64899cb8
Shubham Shubham <shubham4315@gmail.com>
2023-02-21 14:15:46 +0100

    Old boilerplate looks like this

--- Overview/HelloWorld/Program.cs
+++ Overview/HelloWorld/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your Name");
+            string str = Console.ReadLine();
+            Console.WriteLine("Hello " + str);
+        }
+    }
+}
+
