f068496ed1f1c4b1a74fd5a39a4656190fa17eaf
Shubham Shubham <shubham4315@gmail.com>
2023-02-21 14:49:32 +0100

    Operators, nul checking operator ??

--- Overview/Operators/Program.cs
+++ Overview/Operators/Program.cs
@@ -12,11 +12,13 @@ namespace Operators
 
             // TODO: Basic math operators are +, -, /, *
             Console.WriteLine("----- Basic Math -----");
+            Console.WriteLine(x/y);
+            Console.WriteLine(a+b);
 
 
             // TODO: Increment / decrement operators
             Console.WriteLine("----- Shorthand -----");
-
+            Console.WriteLine("{0}  {1}",x++, y--);
 
             // TODO: Operators can be shorthand: a = a + b
 
@@ -28,14 +30,16 @@ namespace Operators
             // null-coalescing operators
             // string str = null;
             // TODO: the ?? operator uses left operand if not null, or right one if it is
+            string str = null;
+            Console.WriteLine(str ?? "The string was empty");
 
-            
             // TODO: the ??= operator assigns the right operand if the left one is null
             // it replaces the code:
             // if (variable is null) {
             //    variable = somevalue;
             // }
-
+            str ??= "was null";
+            Console.WriteLine(str);
         }
     }
 }
